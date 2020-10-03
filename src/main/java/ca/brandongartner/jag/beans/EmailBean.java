9ae3b1983f9b01e926b743e6024c460e50f97096
Brandon Gartner <brandonofthegartner@gmail.com>
2020-10-03 15:48:28 -0400

    accidentally made my todo list on master (not used to things automatically switching to master branch)

---
+++ src/main/java/ca/brandongartner/jag/beans/EmailBean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.brandongartner.jag.beans;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Brandon Gartner
+ */
+public class EmailBean {
+    private final static Logger LOG = LoggerFactory.getLogger(EmailBean.class);
+}
