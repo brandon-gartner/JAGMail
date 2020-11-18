2f2a1041e3842c91cb9a6c6427bb4629f7a38f85
valcon <brandonofthegartner@gmail.com>
2020-11-17 19:26:48 -0500

    finished writing sample tests.

--- src/test/java/ca/brandongartner/jag/mail_database_tests/TestDatabaseMethods.java
+++ src/test/java/ca/brandongartner/jag/mail_database_tests/TestDatabaseMethods.java
@@ -5,25 +5,26 @@
  */
 package ca.brandongartner.jag.mail_database_tests;
 
-import ca.brandongartner.jag.beans.AttachmentBean;
 import ca.brandongartner.jag.beans.EmailBean;
 import ca.brandongartner.jag.beans.MailConfigBean;
 import ca.brandongartner.jag.mail_database.DatabaseDAO;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import jodd.mail.Email;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -46,6 +47,7 @@ public class TestDatabaseMethods {
     MailConfigBean configBean = new MailConfigBean();
     private final static Logger LOG = LoggerFactory.getLogger(TestDatabaseMethods.class);
     private DatabaseDAO instance;
+    private EmailBean basicEmail;
 
     
     
@@ -70,19 +72,16 @@ public class TestDatabaseMethods {
     public void seedDatabase() {
         LOG.info("@Before seeding");
 
-        final String seedDataScript = loadAsString("./CreateTableStructure.sql");
-        try ( Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb?zeroDateTimeBehavior=CONVERT_TO_NULL", "root", "dawson");) {
+        final String seedDataScript = loadAsString("CreateTableStructure.sql");
+        try ( Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb?zeroDateTimeBehavior=CONVERT_TO_NULL", "brandon", "dawson!123");) {
             for (String statement : splitStatements(new StringReader(seedDataScript), ";")) {
                 connection.prepareStatement(statement).execute();
             }
         } catch (SQLException e) {
             throw new RuntimeException("Failed seeding database", e);
         }
-        MailConfigBean configBean = new MailConfigBean();
-        configBean.setMySqlURL("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL");
-        configBean.setMySqlUsername("root");
-        configBean.setMySqlPassword("dawson");
-        instance = new DatabaseDAO(configBean);
+        initialize();
+        basicEmail = createBasicEmailBean();
     }
 
     /**
@@ -96,6 +95,18 @@ public class TestDatabaseMethods {
         }
     }
 
+    private EmailBean createBasicEmailBean(){
+        EmailBean theBean = new EmailBean();
+        Email email = new Email();
+        email.to("bg02test@gmail.com");
+        email.subject("subject1");
+        email.textMessage("aaaaaaaaaaaaaaaa");
+        theBean.setEmail(email);
+        theBean.setFolderId(3);
+        theBean.setEmailId(1);
+        return theBean;
+    }
+    
     private List<String> splitStatements(Reader reader, String statementDelimiter) {
         final BufferedReader bufferedReader = new BufferedReader(reader);
         final StringBuilder sqlStatement = new StringBuilder();
@@ -123,9 +134,197 @@ public class TestDatabaseMethods {
         return line.startsWith("--") || line.startsWith("//") || line.startsWith("/*");
     }
     
-    //1.1 no time to implement
+    public void initialize(){
+        configBean.setUserName("doesTheUserNameDoAnything");
+        configBean.setUserEmailAddress("bg01test@gmail.com");
+        configBean.setPassword("dawson123");
+        configBean.setImapURL("imap.gmail.com");
+        configBean.setSmtpUrl("smtp.gmail.com");
+        configBean.setImapPort("993");
+        configBean.setSmtpPort("465");
+        configBean.setMySqlURL("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL");
+        configBean.setDatabaseName("emailDB");
+        configBean.setMySqlPort("3306");
+        configBean.setMySqlUsername("brandon");
+        configBean.setMySqlPassword("dawson!123");
+        instance = new DatabaseDAO(configBean);
+        
+    }
+    
+    //in class comment: 2-3 per public method, 1 fail at least
+    //list of methods:
+    //insertEmail
+    //createFolder
+    //findEmailsbyEmailAddress
+    //updateEmailFolder
+    //updateDraftEmail
+    //deleteemail
+    //need to do roughly 16-24
+    
+    
+    //findStringInEmail
+    //1.1 finding a string inside of an email
     @Test
-    public void testInsertEmail() throws Exception{
-        throw new Exception("");
+    public void testFindStringInEmail() throws SQLException{
+        LOG.warn(">>>>>>>>>>FIND EMAIL TEST>>>>>>>>>>>>>");
+        ResultSet rs = instance.findStringInEmail("aaaaa");
+        if (rs.next()){
+            assertEquals(getRowCount(rs), 1);
+        }
+        else{
+            LOG.warn("Failed test 1.1");
+            fail();
+        }
+    }
+    
+    //1.2 test email that isn't there, undone
+    @Test
+    public void testFindFakeStringInEmail() throws SQLException{
+        LOG.warn(">>>>>>>>>>FIND EMAIL TEST>>>>>>>>>>>>>");
+        ResultSet rs = instance.findStringInEmail("ewrijiuhweriwiergiouwsiouergiowhrg");
+        if (rs.next()){
+            assertEquals(getRowCount(rs), 0);
+        }
+        else{
+            LOG.warn("Failed test 1.2");
+            fail();
+        }
+    }
+    
+    //1.3 test search for too long string/null
+    @Test(expected = NullPointerException.class)
+    public void testFindNullStringInEmail() throws SQLException{
+        LOG.warn(">>>>>>>>>>FIND EMAIL TEST>>>>>>>>>>>>>");
+        ResultSet rs = instance.findStringInEmail(null);
+    }
+    
+    
+    //findEmailInFolder
+    //2.1 finding the emails in a folder that exists
+    @Test
+    public void findInboxFolder() throws SQLException{
+        ResultSet rs = instance.findEmailInFolder("Inbox");
+        if (rs.next()){
+            assertEquals(getRowCount(rs), 14);
+        }
+        else{
+            LOG.warn("Failed test 2.1");
+            fail();
+        }
+    }
+    
+    //2.2 trying to find emails in a folder that doesn't exist.
+    @Test
+    public void findInbox2Folder() throws SQLException{
+        ResultSet rs = instance.findEmailInFolder("Inbox2");
+        if (rs.next()){
+            fail();
+        }
+        else {
+            assertTrue(true);
+        }
+    }
+    
+    
+    //insertEmail
+    //3.1
+    @Test
+    public void testInsertFineEmail() throws SQLException {
+        basicEmail.setEmailId(25);
+        int rowsAffected = instance.insertEmail(basicEmail, "Inbox");
+        assertEquals(rowsAffected, 2);
+    }
+    //3.2
+    @Test(expected = SQLException.class)
+    public void testInsertBadEmail() throws SQLException {
+        int rowsAffected = instance.insertEmail(basicEmail, "Inbox");
+    }
+    
+    
+    //createFolder
+    //4.1
+    @Test
+    public void testCreateFolder() throws SQLException{
+        int rowsAffected = instance.createFolder("NewFolder");
+        assertEquals(rowsAffected, 1);
+    }
+    //4.2
+    @Test(expected = SQLException.class)
+    public void testCreateFolderTooLongName() throws SQLException {
+        //my SQL database accepts folder names up to 50 characters
+        int rowsAffected = instance.createFolder("thistextisactuallyonecharactertoolongthisshouldfail");
+    }
+    
+    
+    //findEmailsFromEmailAddress
+    //5.1
+    //5.2
+    
+    
+    //updateEmailFolder
+    //6.1
+    @Test
+    public void testUpdateEmailFolder() throws SQLException {
+        int rowsAffected = instance.updateEmailFolder(1, "Inbox");
+        assertEquals(rowsAffected, 1);
+    }
+    //6.2
+    @Test(expected = SQLException.class)
+    public void testUpdateEmailFolderFakeFolder() throws SQLException {
+        int rowsAffected = instance.updateEmailFolder(1, "jxhsdvbfjhgsbuj");
+    }
+    
+    
+    //updateDraftEmail
+    //7.1
+    @Test
+    public void testUpdateDraft() throws SQLException{
+        int rowsAffected = instance.updateDraftEmail(basicEmail);
+        assertEquals(rowsAffected, 4);
+    }
+    //7.2
+    @Test(expected = SQLException.class)
+    public void testUpdateDraftThatIsntDraft() throws SQLException {
+        basicEmail.setEmailId(2147483647);
+        int rowsAffected = instance.updateDraftEmail(basicEmail);
+    }
+    
+    
+    //deleteEmail
+    //8.1
+    @Test
+    public void testDeleteEmail() throws SQLException {
+        int rowsAffected = instance.deleteEmail(1);
+        assertEquals(rowsAffected, 2);
+    }
+    //8.2
+    @Test(expected = SQLException.class)
+    public void testDeleteNonexistentEmail() throws SQLException {
+        int rowsAffected = instance.deleteEmail(2147483647);
+    }
+    
+    
+    
+    //method courtesy of here:
+    //https://stackoverflow.com/questions/8292256/get-number-of-rows-returned-by-resultset-in-java
+    private int getRowCount(ResultSet rs){
+        if (rs == null){
+            return 0;
+        }
+        
+        try{
+            rs.last();
+            return rs.getRow();
+        } catch (SQLException e){
+            LOG.error("SQLException while counting rows of ResultSet.", e);
+        } finally{
+            try{
+                rs.beforeFirst();
+            } catch (SQLException e){
+                LOG.error("SQLException while going to first row of ResultSet.", e);
+            }
+        }
+        
+        return 0;
     }
 }
